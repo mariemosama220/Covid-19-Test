e13658bd6b68cc7983266fe6d8788fc62eb100c8
mariemosama220 <79164138+mariemosama220@users.noreply.github.com>
2022-02-08 23:30:32 +0200

    Add files via upload

---
+++ CovidTestF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.covid.test;
+
+/**
+ *
+ * @author mariem
+ */
+abstract class CovidTestF {
+    CovidTest30 c3=new CovidTest30();
+    CovidTest c1=new CovidTest();
+    CovidTest50 c5=new CovidTest50();
+    
+}
---
+++ Questions.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.covid.test;
+
+/**
+ *
+ * @author mariem
+ */
+public class Questions {
+        private String[][] test=new String[12][4];
+        public void initData3(){
+         test[0][0]="Do you Difficulities in Smelling or Tasting?";
+         test[0][1]="Maybe";test[0][2]="Yes";test[0][3]="No";
+         test[1][0]="Do you have cough?";
+         test[1][1]="Maybe";test[1][2]="Yes";test[1][3]="No";
+         test[2][0]="Do you have colds?";
+         test[2][1]="Maybe";test[2][2]="Yes";test[2][3]="No";
+          test[3][0]="you having Diarrhea?";
+         test[3][1]="Maybe";test[3][2]="Yes";test[3][3]="No";
+         test[4][0]="Do you have sore throat?";
+         test[4][1]="Maybe";test[4][2]="Yes";test[4][3]="No";
+         test[5][0]="Are you experiencing MYALGIA or Body Aches?";
+         test[5][1]="Maybe";test[5][2]="Yes";test[5][3]="No";
+           test[6][0]="Do you have a headache?";
+         test[6][1]="Maybe";test[6][2]="Yes";test[6][3]="No";
+         test[7][0]="Do you have fever (Temperature 37.8 C and above)";
+         test[7][1]="Maybe";test[7][2]="Yes";test[7][3]="No";
+         test[8][0]="Are you having difficulty breathing?";
+         test[8][1]="Maybe";test[8][2]="Yes";test[8][3]="No";
+          test[9][0]="Are you experiencing Fatigue?";
+         test[9][1]="Maybe";test[9][2]="Yes";test[9][3]="No";
+         test[10][0]="Have you traveled recently during the past 14 days?";
+         test[10][1]="Maybe";test[10][2]="Yes";test[10][3]="No";
+      test[11][0]="Do you have High blood pressure?";
+         test[11][1]="Maybe";test[11][2]="Yes";test[11][3]="No";
+     }
+
+    public Questions() {
+    }
+        
+       
+}
